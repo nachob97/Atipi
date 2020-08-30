65da592a2c7b1c33cda32834162eac232737e5e2
Santiago Curi <curisantiago3@gmail.com>
2020-08-29 23:04:11 -0300

    prefiltrado, no funciona correctamente (mucho ruido)

--- main.cpp
+++ main.cpp
@@ -5,17 +5,22 @@
 
 using namespace std;
 
-int main() {
-
-//--------------------------------------------------
+int main(int arguments, char* argv[]) {
+//--------------------------------------------------float delta, string prefilterPath
     clock_t start, end;
     start = clock();
 //--------------------------------------------------
-
+    float delta = strtof(argv[1],nullptr);
     string path = "./ImgDUDE-M-arioSimetrico/Img07M0.05.pgm";
     int cols, rows, range;
     cols = 0; rows = 0;range=0;
     //readHead(path, cols, rows, range);
+    int** prefilter;
+    if (arguments > 2) {
+        string prefilterPath = argv[2];
+        cout << prefilterPath << endl;
+        prefilter = readFile(prefilterPath, cols, rows, range);
+    }
     int** image = readFile(path, cols, rows, range);
     int** extractos = new int* [rows];
     for (int i = 0; i < rows; i++) {
@@ -25,15 +30,19 @@ int main() {
     //ACA VAN 2 FOR PARA SACAR CONTEXTO DE TODOS LOS PIXEL
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            Context* cont = new Context(image, i, j, 8, cols, rows);//valores de prueba para contexto
+            Context* cont = new Context(image, i, j, 4, cols, rows);//valores de prueba para contexto
             int extracto = (int)cont->getExtracto().to_ulong();
             extractos[i][j]= extracto;
-            hc->add(extracto); 
-            hc->sum1(extracto, image[i][j]);
+            hc->add(extracto);
+            if (arguments > 2) {
+                hc->sum1(extracto, prefilter[i][j]); 
+            }
+            else
+                hc->sum1(extracto, image[i][j]);
         }
     }
     //crear la imagen
-    float delta = 0.10;
+    //float delta = 0.10;
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             int prediccion = hc->predict(image[i][j], extractos[i][j], delta);
